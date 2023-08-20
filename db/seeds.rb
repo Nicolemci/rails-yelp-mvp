f7b2ef4cd264475eb25da5e489a3743913f03530
nicole mcinerney <nicolemci@hotmail.com>
2023-08-20 20:11:29 +0100

    complete MVC and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all if Rails.env.development?
+
+Restaurant.create!(
+  name: "Oriental Express",
+  address: "65 Oak Road, London",
+  phone_number: "+44 7878773672",
+  category: "chinese"
+)
+
+Restaurant.create!(
+  name: "Chez Lindsey",
+  address: "45 Convent Garden, London",
+  phone_number: "+44 6573675675",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "San Marco",
+  address: "198 Station Road, Addlestone",
+  phone_number: "+44 2982988668",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "Sushi Samba",
+  address: "35 Liverpool Street",
+  phone_number: "+44 5674836378",
+  category: "japanese"
+)
+
+Restaurant.create!(
+  name: "Sichuan Folk",
+  address: "Shoreditch, London",
+  phone_number: "+441 23 45 67 89",
+  category: "chinese"
+)
